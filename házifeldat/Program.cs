f97278551942ff90f5090759a0da5de886a346b0
Forry04 <83726220+Forry04@users.noreply.github.com>
2022-11-07 17:01:42 +0100

    Add project files.

---
+++ házifeldat/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace házifeldat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string szamok="";
+            string ki;
+            string adatok = "10;7;5";
+
+            foreach (var item in adatok)
+            {
+                if (item==';')
+                {
+                    Console.WriteLine($"{szamok}*2 = {Convert.ToInt32(szamok)*2}");
+                    szamok = "";
+                }
+                else
+                {
+                    szamok = szamok + item;
+                }
+            }
+            Console.WriteLine($"{szamok}*2 = {Convert.ToInt32(szamok) * 2}");
+
+            Console.ReadKey();
+        
+        }
+
+    }
+}
+
